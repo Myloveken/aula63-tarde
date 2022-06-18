128122de6e0ded7f68cf4a3daa2458a7cad3f8cb
Kenlove Chereste <cherestek@gmail.com>
2022-06-18 14:17:10 -0300

    Meu app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
+import './components/UseStateClass'
+import UseStateClass from './components/UseStateClass';
+import UseStateFunction from './components/UseStateFunction';
+import UseEffectClass from './components/UseEffectClass'
+import UseEffectFunction from './components/UseEffectFunction';
+import UseContext from './components/UseContext'
+import UseRef from './components/UseRef.jsx';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        UseStateClass / >
+        <
+        UseStateFunction / >
+        <
+        UseEffectClass / >
+        <
+        UseEffectFunction / >
+        <
+        UseContext / >
+        <
+        UseRef / >
+
+        <
+        /div>
+    );
 }
 
 export default App;
